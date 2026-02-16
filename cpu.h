9e59af21503bde68cc5bed9349ef520330dbc2f2
Edward Lee <e45lee@uwaterloo.ca>
2026-02-16 16:31:37 -0500

    Tests for the CPU.cpp

--- cpu.h
+++ cpu.h
@@ -1,11 +1,20 @@
 /**
     This class represents the overall CPU, which includes the execution engine and any additional components (e.g., coprocessors, interrupt handling).
  */
+ #include "cpu_ee.h"
+
+const uint32_t halted_pc = 0x3fff0000; // A special PC value that indicates the CPU is halted. This is not a valid instruction address.
+
 class CPU {
 private:
     CPU_ExecutionEngine execution_engine; // The execution engine responsible for executing instructions
+    Memory *mem;
 public:
     CPU(Memory* mem, uint32_t initial_pc);
 
-    void step();
+    ExecutionResult step();
+    ExecutionResult stepUntilPC(uint32_t target_pc);
+    uint32_t getRegister(int reg) const;
+    uint32_t getCPSR() const;
+    bool isHalted() const;
 };
